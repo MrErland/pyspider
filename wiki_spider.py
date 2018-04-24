94ab5edfe518bccaa51d24fed2e2afdbad0074d0
Erland <912859459@qq.com>
2018-04-24 20:59:18 +0800

    new wiki spider from one tag

---
+++ wiki_spider.py
@@ -0,0 +1,26 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+import datetime
+import random
+import re
+
+
+def getlinks(articleUrl):
+    try:
+        html = urlopen("http://en.wikipedia.org"+articleUrl)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html)
+    except AttributeError as e:
+        return None
+    return bsObj.find("div", {"id":"bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
+
+random.seed(datetime.datetime.now())
+links = getlinks("/wiki/linux")
+while len(links) > 0:
+    newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
+    print(newArticle)
+    links = getlinks(newArticle)
+
