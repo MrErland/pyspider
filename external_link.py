92c724acdd55b112b3fe906906a32f2b72d074e3
Erland <912859459@qq.com>
2018-04-25 15:44:54 +0800

    modify bug in external_link

--- external_link.py
+++ external_link.py
@@ -37,7 +37,7 @@ def getRandomExternalLink(startingPage):
     externalLinks = getExternalLinks(bsObj, splitAddress(startingPage)[0])
     if len(externalLinks) == 0:
         internalLinks = getInternalLinks(startingPage)
-        return getNextExternalLink(internalLinks[random.randint(0, len(internalLinks)-1)])
+        return getRandomExternalLink(internalLinks[random.randint(0, len(internalLinks)-1)])
     else:
         return externalLinks[random.randint(0, len(externalLinks)-1)]
 
